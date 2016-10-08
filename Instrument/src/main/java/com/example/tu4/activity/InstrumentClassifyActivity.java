a6be9f985ed45fa1c94c310160adacb23f22fc9e
guoxiaoqin <1037930134@qq.com>
2016-10-08 20:12:11 +0800

    gxq的测试

--- Instrument/src/main/java/com/example/tu4/activity/InstrumentClassifyActivity.java
+++ Instrument/src/main/java/com/example/tu4/activity/InstrumentClassifyActivity.java
@@ -48,7 +48,8 @@ public class InstrumentClassifyActivity extends AppCompatActivity {
         initTabLayout();
         initMaterialRefreshLayout();
         Toast.makeText(this, "测试", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "测试", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "测试2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "测试3", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "guoxiaoqin", Toast.LENGTH_SHORT).show();
         // initRecycle();
         // initRecycle();
