e8c6b3633ed5bf9f896e28bc8da7b7be45b69a70
wenuser <1961947276@qq.com>
2016-10-13 15:11:00 +0800

    增加课程订购页面，格式化代码

--- Instrument/src/main/java/com/example/tu4/activity/SubjectDetailActivity.java
+++ Instrument/src/main/java/com/example/tu4/activity/SubjectDetailActivity.java
@@ -1,32 +1,43 @@
 package com.example.tu4.activity;
 
+import static com.example.tu4.model.AplicationStatic.MAX_STUDENT_NUMBER;
+import static com.example.tu4.model.AplicationStatic.MAX_STUDENT_NUMBER_BACK;
+import static com.example.tu4.model.AplicationStatic.STUDENT_NUMBER;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.tu4.R;
 import com.example.tu4.view.CircleImageView;
 
-import static com.example.tu4.model.AplicationStatic.MAX_STUDENT_NUMBER;
-import static com.example.tu4.model.AplicationStatic.MAX_STUDENT_NUMBER_BACK;
-import static com.example.tu4.model.AplicationStatic.STUDENT_NUMBER;
-
 public class SubjectDetailActivity extends AppCompatActivity {
-
 
     LinearLayout mLinearLayout, mLinearLayoutFeedback;
     LayoutInflater mInflater = null;
+    private TextView money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_detail);
+        money = (TextView) findViewById(R.id.tv_money_subjectdetail);
         mInflater = LayoutInflater.from(this);
         mLinearLayout = (LinearLayout) findViewById(R.id.linearlayout_studenimage_subjectdetail);
         mLinearLayoutFeedback = (LinearLayout) findViewById(
                 R.id.linearlayout_studentback_subjectdetail);
+        money.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(SubjectDetailActivity.this, CourseSubscribeActivity.class);
+                startActivity(intent);
+            }
+        });
         initLinearlayoutImage();
         initLinearlayouFeedback();
     }
@@ -59,5 +70,6 @@ public class SubjectDetailActivity extends AppCompatActivity {
             }
         }
     }
+
 
 }
