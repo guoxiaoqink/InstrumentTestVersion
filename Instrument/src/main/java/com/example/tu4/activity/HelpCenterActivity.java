4e0e64bbe4baeb0046812d78c5c1542add0e9200
HuashanOK <1834497859@qq.com>
2016-10-14 11:09:19 +0800

    华珊测试

---
+++ Instrument/src/main/java/com/example/tu4/activity/HelpCenterActivity.java
@@ -0,0 +1,43 @@
+package com.example.tu4.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.tu4.R;
+import com.example.tu4.adapter.HelpCenterListviewAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class HelpCenterActivity extends AppCompatActivity {
+
+    @BindView(R.id.img_my_works_return)
+    ImageView imgMyWorksReturn;
+    private String[] title = {"标题", "标题", "标题",};
+    private String[] text = {"内容1", "内容2", "内容3",};
+
+
+    @BindView(R.id.lv_help_center)
+    ListView lvHelpCenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_center);
+        ButterKnife.bind(this);
+
+        lvHelpCenter.setAdapter(new HelpCenterListviewAdapter(this, title, text));
+
+
+    }
+
+    @OnClick(R.id.img_my_works_return)
+    public void onClick() {
+        Intent intent = new Intent(HelpCenterActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
