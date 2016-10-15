bb756357f714c9a28459c79135244d621424856e
guoxiaoqin <1037930134@qq.com>
2016-10-15 15:19:49 +0800

    添加httputilsmodel依赖

---
+++ Instrument/src/main/java/com/example/tu4/okhttp/JsonGenericsSerializator.java
@@ -0,0 +1,16 @@
+package com.example.tu4.okhttp;
+
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.callback.IGenericsSerializator;
+
+/**
+ * Created by JimGong on 2016/6/23.
+ */
+public class JsonGenericsSerializator implements IGenericsSerializator {
+    Gson mGson = new Gson();
+
+    @Override
+    public <T> T transform(String response, Class<T> classOfT) {
+        return mGson.fromJson(response, classOfT);
+    }
+}
