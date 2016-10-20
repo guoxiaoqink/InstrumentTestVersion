ec79ac5f2dd82e8d6cc0f096dd697a28fb48125e
wqjuser <316400644@qq.com>
2016-10-20 08:53:06 +0800

    复制图片到不同文件夹

---
+++ Instrument/src/main/java/com/example/tu4/activity/IssiuFeedbackActivity.java
@@ -0,0 +1,18 @@
+package com.example.tu4.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.PopupWindow;
+
+import com.example.tu4.R;
+
+public class IssiuFeedbackActivity extends AppCompatActivity {
+    private PopupWindow popupWindow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_issiu_feedback);
+
+    }
+}
