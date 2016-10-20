63ce636beb5c3b21aa11cec2a50b100358a90a1d
suchunyu <1792156002@qq.com>
2016-10-20 14:36:40 +0800

    苏春雨乐器详情的修改

--- Instrument/src/main/java/com/example/tu4/activity/InstrumentDetailsActivity.java
+++ Instrument/src/main/java/com/example/tu4/activity/InstrumentDetailsActivity.java
@@ -1,11 +1,13 @@
 package com.example.tu4.activity;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.tu4.R;
 
@@ -17,12 +19,15 @@ public class InstrumentDetailsActivity extends AppCompatActivity {
     @BindView(R.id.img_return)
     ImageView imgReturn;
     private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrument_details);
         ButterKnife.bind(this);
+        textView= (TextView) findViewById(R.id.tv_old_level);
+        textView.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG); //中划线
 
         button = (Button) findViewById(R.id.btn_buy);
         button.setOnClickListener(new View.OnClickListener() {
