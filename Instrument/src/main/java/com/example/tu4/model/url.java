59b3f0482b7b5856725fee2d0179eb83c15af439
guoxiaoqin <1037930134@qq.com>
2016-10-25 14:38:37 +0800

    url地址

---
+++ Instrument/src/main/java/com/example/tu4/model/url.java
@@ -0,0 +1,12 @@
+package com.example.tu4.model;
+
+/**
+ * Created by Adelais on 2016/10/25.
+ * Descripyion: 这个类用来做什么
+ * Version：1
+ * Modify Person：   on
+ */
+
+public interface Url {
+    String url = "http://138.68.11.233:8080/";
+}
